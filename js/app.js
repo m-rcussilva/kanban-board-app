fe73aa7fda4004ce00c02b44b85ef44b5853411b
marcusluis <marcusluis.mail@gmail.com>
2022-07-12 18:25:55 -0300

    funcao para adc tarefa e deletar tarefa criadas

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("form");
+const newTask = document.getElementById("new-task");
+const root = document.getElementById("root");
+
+const generateTemplate = (task) => {
+    const htmlTemplate = `
+        <li class="task-li">
+            ${task}
+            <i class="fa-solid fa-xmark" title="Apagar tarefa"></i>
+        </li>
+    `;
+
+    root.innerHTML += htmlTemplate;
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const task = newTask.value.trim();
+
+    if (!task) {
+        alert("O campo 'Nova tarefa' parece estar vazio. Por favor, adicione uma mensagem.");
+        return;
+    }
+
+    generateTemplate(task);
+
+    newTask.value = "";
+});
+
+root.addEventListener("click", (e) => {
+    if (e.target.classList.contains("fa-xmark")) {
+        e.target.parentElement.remove();
+    }
+});
